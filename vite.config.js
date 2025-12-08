6bbce6bf157c2f5d566d671e7394439ec99e79b7
chun50405 <chun50405@gmail.com>
2025-12-08 23:38:02 +0800

    增加一下環境變數

--- vite.config.js
+++ vite.config.js
@@ -6,18 +6,16 @@ import tailwindcss from '@tailwindcss/vite'
 // export default defineConfig({
 //   plugins: [vue()],
 // })
-export default defineConfig({
-  plugins: [
-    vue(),
-    tailwindcss()
-  ],
+
+export default defineConfig(({ mode }) => ({
+  plugins: [vue(), tailwindcss()],
   server: {
-    proxy: {
+    proxy: mode === 'development' ? {
       '/api': {
-        target: 'https://script.google.com/macros/s/AKfycbwrsz8T1TkPfaNqZi4rodQVcllOYJWI99UG03-45GW8bHOa5A0aHkUFtVgqdpkFyF2b/exec',
+        target: "https://script.google.com/macros/s/AKfycbwrsz8T1TkPfaNqZi4rodQVcllOYJWI99UG03-45GW8bHOa5A0aHkUFtVgqdpkFyF2b/exec",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
-    }
+    } : undefined
   }
-})
+}))
